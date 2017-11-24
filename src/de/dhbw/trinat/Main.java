7452fea22be6f80400ee32d8079e210fcf8f9243
Elias Kaufmann <kaufmi_95@hotmail.com>
2017-11-24 08:56:58 +0100

    inital

---
+++ src/de/dhbw/trinat/Main.java
@@ -0,0 +1,12 @@
+package de.dhbw.trinat;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Trinat");
+		
+	}
+
+}
